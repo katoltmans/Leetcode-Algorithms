0f7c0e28ca6b51cada0b41e37c572da2aafe42d8
katoltmans <katoltmans@msn.com>
2022-08-10 23:10:16 -0400

    Add camelCase solution

--- HackerRank/week1.js
+++ HackerRank/week1.js
@@ -1,3 +1,12 @@
+{
+    /* 
+    Plus Minus - 
+    Given an array of integers, calculate the ratios of its elements that are 
+    positive, negative, and zero. Print the decimal value of each fraction on 
+    a new line with  places after the decimal.
+    */
+}
+
 function plusMinus(arr) {
     let positive = 0;
     let zero = 0;
@@ -14,13 +23,22 @@ function plusMinus(arr) {
     }
 
     positive = positive / arr.length;
-    console.log(positive);
+    console.log(positive.toFixed(6));
     negative = negative / arr.length;
-    console.log(negative);
+    console.log(negative.toFixed(6));
     zero = zero / arr.length;
-    console.log(zero);
+    console.log(zero.toFixed(6));
 
     return positive, negative, zero;
+}
+
+plusMinus([-4, 3, -9, 0, 4, 1]);
+
+{
+    /* 
+    Min-Max Sum - 
+
+*/
 }
 
 function miniMaxSum(arr) {
@@ -42,4 +60,144 @@ function miniMaxSum(arr) {
     let minMax = console.log(min, max);
 
     return minMax;
+}
+
+{
+    /*  */
+}
+
+function timeConversion(s) {
+    let hour = s.slice(0, 1);
+    console.log(hour);
+}
+
+timeConversion("07:05:45PM");
+
+{
+    /*  */
+}
+
+{
+    /* Your task is to write a program that creates or splits Camel Case variable, method, and class names.
+    Sample Input
+
+S;M;plasticCup()
+
+C;V;mobile phone
+
+C;C;coffee machine
+
+S;C;LargeSoftwareBook
+
+C;M;white sheet of paper
+
+S;V;pictureFrame
+
+Sample Output
+
+plastic cup
+
+mobilePhone
+
+CoffeeMachine
+
+large software book
+
+whiteSheetOfPaper()
+
+picture frame
+    
+    */
+}
+
+function processData(inputBlock) {
+    //console.log("'"+inputBlock.replace(/\n/g, '\\n').replace(/\r/g, '\\r')+"'");
+    //let cleanInput = inputBlock.replace(/\r/g, '');
+    let inputArray = inputBlock.split("\r\n");
+    let outputArray = [];
+
+    for (let input of inputArray) {
+        let newString = "";
+        if (input[0] === "S") {
+            if (input[2] === "M") {
+                for (let i = 4; i < input.length - 2; i++) {
+                    if (input[i] === input[i].toUpperCase()) {
+                        newString += " " + input[i].toLowerCase();
+                    } else {
+                        newString += input[i];
+                    }
+                    if (input[i] === "(") {
+                        return newString;
+                    }
+                }
+                // console.log(newString);
+            }
+            if (input[2] === "C") {
+                newString += input[4].toLowerCase();
+                for (let i = 5; i < input.length; i++) {
+                    if (input[i] === input[i].toUpperCase() && i) {
+                        newString += " " + input[i].toLowerCase();
+                    } else {
+                        newString += input[i];
+                    }
+                }
+                // console.log(newString);
+            }
+            if (input[2] === "V") {
+                for (let i = 4; i < input.length; i++) {
+                    if (input[i] === input[i].toUpperCase() && i) {
+                        newString += " " + input[i].toLowerCase();
+                    } else {
+                        newString += input[i];
+                    }
+                }
+                // console.log(newString);
+            }
+        }
+        if (input[0] === "C") {
+            if (input[2] === "C") {
+                newString += input[4].toUpperCase();
+                for (let i = 5; i < input.length; i++) {
+                    if (input[i] === " ") {
+                        i++;
+                        newString += input[i].toUpperCase();
+                    } else {
+                        newString += input[i];
+                    }
+                }
+                // console.log(newString);
+            }
+            if (input[2] === "V" || input[2] === "M") {
+                for (let i = 4; i < input.length; i++) {
+                    if (input[i] === " ") {
+                        i++;
+                        newString += input[i].toUpperCase();
+                    } else {
+                        newString += input[i];
+                    }
+                }
+                if (input[2] === "M") {
+                    newString += "()";
+                }
+                // console.log(newString);
+            }
+        }
+
+        outputArray.push(newString);
+    }
+    let finalString = outputArray.join("\n");
+    console.log(finalString);
+    return finalString;
+}
+
+{
+    /*  */
+}
+
+{
+    /*  */
+}
+
+{
+    /*  */
 }
