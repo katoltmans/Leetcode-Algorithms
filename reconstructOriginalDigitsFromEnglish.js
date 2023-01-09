bdb7b087e25ea0185612a1e9e1cb5c01ab0c82da
katoltmans <katoltmans@msn.com>
2023-01-08 22:43:40 -0500

    Add "Reconstruct Original Digits from English" algo.
    Determine the numbers present in a mix of letters from their word form and return the numbers in ascending order.

---
+++ reconstructOriginalDigitsFromEnglish.js
@@ -0,0 +1,78 @@
+{
+    /*
+    Reconstruct Original Digits from English
+
+    Given a string s containing an out-of-order English representation of digits 0-9, return the digits in ascending order.
+     */
+}
+
+var originalDigits = function (s) {
+    // Create an object to keep track of a count of the corresponding numbers to words found
+    // Look only for characters with unique letters (2, 4, 6, 8, 10)
+    // Go through a second round and find the most unique characters of the remaining numbers
+
+    let numsCount = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+    };
+
+    // Add a count to the corresponding number when finding the following letters
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "z") {
+            numsCount[0]++;
+        } else if (s[i] === "w") {
+            numsCount[2]++;
+        } else if (s[i] === "u") {
+            numsCount[4]++;
+        } else if (s[i] === "x") {
+            numsCount[6]++;
+        } else if (s[i] === "g") {
+            numsCount[8]++;
+        } else if (s[i] === "o") {
+            numsCount[1]++;
+        } else if (s[i] === "h") {
+            numsCount[3]++;
+        } else if (s[i] === "f") {
+            numsCount[5]++;
+        } else if (s[i] === "s") {
+            numsCount[7]++;
+        } else if (s[i] === "i") {
+            numsCount[9]++;
+        }
+    }
+    // console.log("NUMS COUNT:", numsCount);
+
+    // Unique letter counts will remain as is - calculate the counts of numbers with nonunique letters by subtracting the unique and previous letter counts
+    numsCount[1] -= numsCount[0] + numsCount[2] + numsCount[4];
+    numsCount[3] -= numsCount[8];
+    numsCount[5] -= numsCount[4];
+    numsCount[7] -= numsCount[6];
+    numsCount[9] -= numsCount[6] + numsCount[8] + numsCount[5];
+
+    // console.log("NEW NUMS COUNT:", numsCount);
+
+    // Create the string of integers by concatinating the key to it the number of times of the value
+    let result = "";
+
+    for (let j = 0; j < 10; j++) {
+        while (numsCount[j] !== 0) {
+            result += j;
+            numsCount[j]--;
+            // console.log("RESULT:", result);
+        }
+    }
+
+    return result;
+};
+
+console.log("Reconstruct Original Digits from English");
+console.log(originalDigits("owoztneoer")); // expect "012"
+console.log(originalDigits("fviefuro")); // expect "45"
